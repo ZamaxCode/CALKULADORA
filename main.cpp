b8d2da0318f8a68886ca21f84ef7cc471423da19
ZamaxCode <zarate.alejandroM14@live.com.mx>
2021-06-04 14:50:12 -0500

    Diseño interfaz definitivo

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    QFile styleSheetFile(":/Diseño/Devsion.qss");
+    QFile styleSheetFile(":/Diseño/SyNet.qss");
     styleSheetFile.open(QFile::ReadOnly);
     QString styleSheet = QLatin1String(styleSheetFile.readAll());
     a.setStyleSheet(styleSheet);
